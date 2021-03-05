95719c9689da9827d80ac7892b6037f330c36e49
Murat Altınpınar <murtekbey@gmail.com>
2021-03-06 00:51:46 +0300

    Add project files.

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new LoggerFactory());
+            customerManager.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class LoggerFactory : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            return new MurtekLogger();
+        }
+    }
+
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    public interface ILogger
+    {
+        void Log();
+    }
+
+    public class MurtekLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with MurtekLogger");
+        }
+    }
+
+    public class CustomerManager
+    {
+        ILoggerFactory _loggerFactory;
+
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        public void Save()
+        {
+            Console.WriteLine("Saved");
+            ILogger logger = _loggerFactory.CreateLogger();
+            logger.Log();
+        }
+    }
+}
